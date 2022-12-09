1c578d1fe3d1c6218d579f31935cccd1387a968d
s-webdev <59814096+s-webdev@users.noreply.github.com>
2022-12-09 17:43:09 +0300

    - designed desktop services window

--- src/pages/DesktopPages/DesktopServicesPage/DesktopServicesPage.tsx
+++ src/pages/DesktopPages/DesktopServicesPage/DesktopServicesPage.tsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { Window, TitleBar } from "react-desktop/macOs";
+import MobileIcon from "../../../assets/icons/services/mobile-icon.svg";
+import IntegrationsIcon from "../../../assets/icons/services/integrations-icon.svg";
+import AnalyticsIcon from "../../../assets/icons/services/analytics-icon.svg";
+import SEOIcon from "../../../assets/icons/services/seo-icon.svg";
+
+const services = [
+  {name: "Web Applications", icon: MobileIcon},
+  {name: "Mobile Applications", icon: MobileIcon},
+  {name: "E-Commerce", icon: MobileIcon},
+  {name: "UI / UX", icon: MobileIcon},
+  {name: "Integrations", icon: IntegrationsIcon},
+  {name: "Analytics", icon: AnalyticsIcon},
+  {name: "SEO", icon: SEOIcon},
+];
 
 const DesktopServicesPage = () => {
   return (
-    <div>
-      Desktop Services
-    </div>
+    <Window
+      chrome
+      width={808}
+      height={517}
+      className="absolute"
+    >
+      <TitleBar
+        title="Services"
+        controls
+      />
+      <div className="w-full grid grid-cols-4">
+        {services.map((service, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center"
+            >
+              <img
+                src={service.icon}
+                alt={`${service.name} icon`}
+              />
+              {service.name}
+            </div>
+          )
+        })}
+      </div>
+    </Window>
   )
 }
 
