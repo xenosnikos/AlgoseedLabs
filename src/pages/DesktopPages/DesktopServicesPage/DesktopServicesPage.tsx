fe8c06d28fd79e5b926d349ae2596f80cc2c11a2
s-webdev <59814096+s-webdev@users.noreply.github.com>
2023-01-11 07:56:21 -0500

    - updated services page for responsive in window

--- src/pages/DesktopPages/DesktopServicesPage/DesktopServicesPage.tsx
+++ src/pages/DesktopPages/DesktopServicesPage/DesktopServicesPage.tsx
@@ -27,7 +27,7 @@ const DesktopServicesPage = () => {
         title="Services"
         controls
       />
-      <div className="w-full grid grid-cols-4">
+      <div className="w-full h-fit grid grid-cols-4">
         {services.map((service, index) => {
           return (
             <div
--- src/pages/FullPages/FullServicesPage/FullServicesPage.tsx
+++ src/pages/FullPages/FullServicesPage/FullServicesPage.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import cn from "classnames";
+import MobileIcon from "../../../assets/icons/services/mobile-icon.svg";
+import IntegrationsIcon from "../../../assets/icons/services/integrations-icon.svg";
+import AnalyticsIcon from "../../../assets/icons/services/analytics-icon.svg";
+import SEOIcon from "../../../assets/icons/services/seo-icon.svg";
 
-const FullServicesPage = () => {
+const services = [
+  {name: "Web Applications", icon: MobileIcon},
+  {name: "Mobile Applications", icon: MobileIcon},
+  {name: "E-Commerce", icon: MobileIcon},
+  {name: "UI / UX", icon: MobileIcon},
+  {name: "Integrations", icon: IntegrationsIcon},
+  {name: "Analytics", icon: AnalyticsIcon},
+  {name: "SEO", icon: SEOIcon},
+];
+
+const FullServicesPage = (props: any) => {
   return (
-    <div>
-      This Page is empty.
+    <div className={cn("w-full h-fit grid  gap-y-20",
+      props.windowSize.width > 720 ? "grid-cols-4" : props.windowSize.width > 500 ? "grid-cols-3" : props.windowSize.width > 360 ? "grid-cols-2" : "grid-cols-1"
+    )}>
+      {services.map((service, index) => {
+        return (
+          <div
+            key={index}
+            className="flex flex-col items-center"
+          >
+            <img
+              src={service.icon}
+              alt={`${service.name} icon`}
+            />
+            {service.name}
+          </div>
+        )
+      })}
     </div>
   )
 }
