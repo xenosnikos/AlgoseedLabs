238cd255d6d938ff78fe895e02506ce91587d5fc
s-webdev <59814096+s-webdev@users.noreply.github.com>
2023-01-12 02:14:24 -0500

    - removed unnecessary codes and refactored code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,19 +6,11 @@ import {
 import EnterContainer from "../containers/EnterContainer";
 import DesktopContainer from "../containers/DesktopContainer";
 import WindowsContainer from "../containers/WindowsContainer";
-import FullWindowContainer from "../containers/FullWindowContainer";
 import EnterIndexPage from "../pages/EnterPages/EnterIndexPage";
 import EnterLoadingFirstPage from "../pages/EnterPages/EnterLoadingFirstPage";
 import EnterLoadingSecondPage from "../pages/EnterPages/EnterLoadingSecondPage";
 import DesktopIndexPage from "../pages/DesktopPages/DesktopIndexPage";
 import DesktopServicesPage from "../pages/DesktopPages/DesktopServicesPage";
-import FullHomePage from "../pages/FullPages/FullHomePage";
-import FullServicesPage from "../pages/FullPages/FullServicesPage";
-import FullAboutPage from "../pages/FullPages/FullAboutPage";
-import FullContactUsPage from "../pages/FullPages/FullContactUsPage";
-import FullPortfolioPage from "../pages/FullPages/FullPortfolioPage";
-import FullCaseStudiesPage from "../pages/FullPages/FullCaseStudiesPage";
-import FullSupportPage from "../pages/FullPages/FullSupportPage";
 import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
@@ -64,46 +56,7 @@ const router = createBrowserRouter([
   {
     path: "/windows",
     element: <WindowsContainer />
-  },
-  // {
-  //   path: "/full",
-  //   element: <FullWindowContainer />,
-  //   errorElement: <ErrorPage />,
-  //   children: [
-  //     {
-  //       index: true,
-  //       element: <Navigate to="/full/home" replace />
-  //     },
-  //     {
-  //       path: "/full/home",
-  //       element: <FullHomePage />
-  //     },
-  //     {
-  //       path: "/full/services",
-  //       element: <FullServicesPage />
-  //     },
-  //     {
-  //       path: "/full/about",
-  //       element: <FullAboutPage />
-  //     },
-  //     {
-  //       path: "/full/contact",
-  //       element: <FullContactUsPage />
-  //     },
-  //     {
-  //       path: "/full/portfolio",
-  //       element: <FullPortfolioPage />
-  //     },
-  //     {
-  //       path: "/full/caseStudies",
-  //       element: <FullCaseStudiesPage />
-  //     },
-  //     {
-  //       path: "/full/support",
-  //       element: <FullSupportPage />
-  //     }
-  //   ]
-  // }
+  }
 ]);
 
 export default router
